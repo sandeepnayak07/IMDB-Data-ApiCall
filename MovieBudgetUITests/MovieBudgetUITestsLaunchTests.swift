891c9e865d5832fc44d0c20b913cbdc335aad7db
Sandeep <sandeep.megavath@westagilelabs.com>
2023-04-15 11:58:23 +0530

    Initial Commit

---
+++ MovieBudgetUITests/MovieBudgetUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieBudgetUITestsLaunchTests.swift
+//  MovieBudgetUITests
+//
+//  Created by West Agile labs on 15/04/23.
+//
+
+import XCTest
+
+final class MovieBudgetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
